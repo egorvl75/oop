985c49f1151031928335d3192abd622207f94629
egor.vl <egor.vagichev@gmail.com>
2020-10-01 17:23:40 +0300

    draw

--- ConsoleApp37/ConsoleApp37/Program.cs
+++ ConsoleApp37/ConsoleApp37/Program.cs
@@ -10,15 +10,24 @@ namespace ConsoleApp37
             int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.WriteLine(sym1);
+            Draw(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
-            Console.SetCursorPosition(x2, y2);
-            Console.WriteLine(sym2);
+            Draw(x2, y2, sym2);
+
+            int x3 = 10;
+            int y3 = 15;
+            char sym3 = '$';
+            Draw(x3, y3, sym3);
             Console.ReadLine();
+
+        }
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
